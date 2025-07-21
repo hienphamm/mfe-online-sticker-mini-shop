bef5abf997f70ab8037c3b238d9a32146209cf81
Hien Pham <hien.phamminh2001@gmail.com>
2025-07-21 22:27:11 +0700

    refactor: move shell files to root directory and update module federation config for production

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -1,10 +1,12 @@
 import { createModuleFederationConfig } from '@module-federation/enhanced/rspack';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default createModuleFederationConfig({
     name: 'shell',
     remotes: {
-        order: 'order@http://localhost:8080/mf-manifest.json',
-        product: 'product@http://localhost:8082/mf-manifest.json',
+        order: `order@${isProd ? 'https://mfe-angular-order.vercel.app' : 'http://localhost:8080'}/mf-manifest.json`,
+        product: `product@${isProd ? 'https://mfe-vue-product.vercel.app' : 'http://localhost:8082'}/mf-manifest.json`,
     },
     exposes: {},
     shared: {
