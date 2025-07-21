2a6925d14f9d0189669fdc780dde3f3f71bcccc8
Hien Pham <hien.phamminh2001@gmail.com>
2025-07-21 22:35:46 +0700

    chore: comment out order remote in module federation config and add vercel configuration

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 export default createModuleFederationConfig({
     name: 'shell',
     remotes: {
-        order: `order@${isProd ? 'https://mfe-angular-order.vercel.app' : 'http://localhost:8080'}/mf-manifest.json`,
+        // order: `order@${isProd ? 'https://mfe-angular-order.vercel.app' : 'http://localhost:8080'}/mf-manifest.json`,
         product: `product@${isProd ? 'https://mfe-vue-product.vercel.app' : 'http://localhost:8082'}/mf-manifest.json`,
     },
     exposes: {},
